dd80ff9a459f33bf8c5a385d5d126288318ede64
fuhaiwei <fuhaiwei@126.com>
2017-01-24 20:43:35 +0800

    feat: update security config

--- src/main/java/mingzuozhibi/persist/core/User.java
+++ src/main/java/mingzuozhibi/persist/core/User.java
@@ -3,38 +3,23 @@ package mingzuozhibi.persist.core;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import mingzuozhibi.persist.BaseModel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Date;
 
 @Entity
-public class User extends BaseModel implements UserDetails {
+public class User extends BaseModel {
 
     private String password;
     private String username;
-    private String userRoles;
-
     private boolean enabled;
-    private boolean accountNonLocked;
-    private boolean accountNonExpired;
-    private boolean credentialsNonExpired;
 
     private Date registerDate;
     private Date lastLoggedin;
 
     public User() {
-        userRoles = "ROLE_BASIC";
         enabled = true;
-        accountNonLocked = true;
-        accountNonExpired = true;
-        credentialsNonExpired = true;
         registerDate = new Date();
     }
 
@@ -57,15 +42,6 @@ public class User extends BaseModel implements UserDetails {
         this.username = username;
     }
 
-    @Column(length = 100)
-    public String getUserRoles() {
-        return userRoles;
-    }
-
-    public void setUserRoles(String userRoles) {
-        this.userRoles = userRoles;
-    }
-
     @Column
     public boolean isEnabled() {
         return enabled;
@@ -73,33 +49,6 @@ public class User extends BaseModel implements UserDetails {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
-    }
-
-    @Column
-    public boolean isAccountNonLocked() {
-        return accountNonLocked;
-    }
-
-    public void setAccountNonLocked(boolean accountNonLocked) {
-        this.accountNonLocked = accountNonLocked;
-    }
-
-    @Column
-    public boolean isAccountNonExpired() {
-        return accountNonExpired;
-    }
-
-    public void setAccountNonExpired(boolean accountNonExpired) {
-        this.accountNonExpired = accountNonExpired;
-    }
-
-    @Column
-    public boolean isCredentialsNonExpired() {
-        return credentialsNonExpired;
-    }
-
-    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
-        this.credentialsNonExpired = credentialsNonExpired;
     }
 
     @Column(nullable = false)
@@ -120,21 +69,6 @@ public class User extends BaseModel implements UserDetails {
 
     public void setLastLoggedin(Date lastLoggedin) {
         this.lastLoggedin = lastLoggedin;
-    }
-
-    @Transient
-    @JsonIgnore
-    public List<GrantedAuthority> getAuthorities() {
-        Logger logger = LoggerFactory.getLogger(this.getClass());
-        logger.debug("检查访问权限: 用户: %s, 权限: %s ", username, userRoles);
-
-        if (userRoles == null) {
-            return new ArrayList<>(0);
-        }
-
-        return Arrays.stream(userRoles.split(","))
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/mingzuozhibi/security/SecurityConfiguration.java
+++ src/main/java/mingzuozhibi/security/SecurityConfiguration.java
@@ -5,16 +5,22 @@ import mingzuozhibi.persist.core.UserDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -25,31 +31,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDao userDao;
 
-    @Autowired
-    private Environment env;
+    @Value("${security.admin.password}")
+    private String securityAdminPassword;
+
+    @Value("${security.admin.userlist}")
+    private String securityAdminUserlist;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        Logger logger = LoggerFactory.getLogger(this.getClass());
-
-        String password = env.getProperty("security.admin.password");
-
-        User user = userDao.findByUsername("admin");
-        if (user == null) {
-            user = new User();
-            user.setUsername("admin");
-            user.setPassword(password);
-            userDao.save(user);
-
-            logger.info("创建管理员用户");
-        } else if (!password.equals(user.getPassword())) {
-            user.setPassword(password);
-            userDao.save(user);
-
-            logger.info("更新管理员密码");
-        }
-
-        auth.userDetailsService(userDao::findByUsername);
+        setupAdminUser();
+        auth.userDetailsService(new MyUserDetailsService());
     }
 
     @Override
@@ -57,13 +48,94 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         Logger logger = LoggerFactory.getLogger(this.getClass());
 
         http.authorizeRequests()
+                .antMatchers("/api/users/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET).permitAll()
-                .antMatchers(HttpMethod.POST, "/api/users").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/api/**").hasRole("USER");
         http.httpBasic();
         http.csrf().disable();
 
         logger.info("设置安全策略");
     }
 
+    private void setupAdminUser() {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        User user = userDao.findByUsername("admin");
+        if (user == null) {
+            user = new User();
+            user.setUsername("admin");
+            user.setPassword(securityAdminPassword);
+            userDao.save(user);
+
+            logger.info("创建管理员用户");
+        } else if (!securityAdminPassword.equals(user.getPassword())) {
+            user.setPassword(securityAdminPassword);
+            userDao.save(user);
+
+            logger.info("更新管理员密码");
+        }
+    }
+
+
+    private class MyUserDetailsService implements UserDetailsService {
+
+        private final List<GrantedAuthority> ADMIN_ROLES;
+        private final List<GrantedAuthority> USER_ROLES;
+        private final HashSet<String> ADMIN_LIST;
+
+        private MyUserDetailsService() {
+            ADMIN_ROLES = Stream.of("ROLE_USER", "ROLE_ADMIN")
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+            USER_ROLES = Stream.of("ROLE_USER")
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+            ADMIN_LIST = Stream.of(securityAdminUserlist.split(","))
+                    .collect(Collectors.toCollection(HashSet::new));
+        }
+
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            User user = userDao.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException("username " + username + " not found");
+            }
+            return new UserDetails() {
+                public List<GrantedAuthority> getAuthorities() {
+                    onLoginSuccess();
+                    return ADMIN_LIST.contains(username) ? ADMIN_ROLES : USER_ROLES;
+                }
+
+                private void onLoginSuccess() {
+                    user.setLastLoggedin(new Date());
+                    userDao.save(user);
+                }
+
+                public String getPassword() {
+                    return user.getPassword();
+                }
+
+                public String getUsername() {
+                    return user.getUsername();
+                }
+
+                public boolean isEnabled() {
+                    return user.isEnabled();
+                }
+
+                public boolean isAccountNonLocked() {
+                    return true;
+                }
+
+                public boolean isAccountNonExpired() {
+                    return true;
+                }
+
+                public boolean isCredentialsNonExpired() {
+                    return true;
+                }
+            };
+        }
+
+    }
 }
