4f023cb965c3a04f8ace5ab9bcfdea1a57d880a0
fuhaiwei <fuhaiwei@126.com>
2017-01-29 14:08:29 +0800

    feat: add ajax logout status

--- src/main/java/mingzuozhibi/action/LoginController.java
+++ src/main/java/mingzuozhibi/action/LoginController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,11 +42,44 @@ public class LoginController {
                 return "{\"success\": true}";
             } else {
                 logger.info("用户登入: 未能成功登入, username={}", username);
+                return "{\"success\": false}";
             }
         } catch (UsernameNotFoundException ignored) {
             logger.info("用户登入: 未找到该用户, username={}", username);
+            return "{\"success\": false}";
         }
-        return "{\"success\": false}";
+    }
+
+    @PostMapping("/api/logout")
+    public String ajaxLogout() {
+        logger.info("用户登出: 正在检测登入状态");
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            authentication.setAuthenticated(false);
+            logger.info("用户登出: 用户已成功登出, username={}", authentication.getName());
+            return "{\"success\": true}";
+        } else {
+            logger.info("用户登出: 未检测到已登入状态");
+            return "{\"success\": false}";
+        }
+    }
+
+    @PostMapping("/api/status")
+    public String ajaxStatus() {
+        logger.info("状态获取: 正在检测登入状态");
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            logger.info("状态获取: 检测到已登入用户, username={}", username);
+            return "{\"success\": true, \"username\": \"" + username + "\"}";
+        } else {
+            logger.info("状态获取: 未检测到已登入状态");
+            return "{\"success\": false}";
+        }
     }
 
     private void onLoginSuccess(String username) {
