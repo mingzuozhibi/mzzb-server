c8eb844dd87652ba575c593cb1ffee42320031d1
fuhaiwei <fuhaiwei@126.com>
2017-01-29 14:47:06 +0800

    fix: do check anonymous user

--- src/main/java/mingzuozhibi/action/AuthController.java
+++ src/main/java/mingzuozhibi/action/AuthController.java
@@ -30,6 +30,7 @@ public class AuthController {
     public String login(@JsonArg("$.username") String username,
                         @JsonArg("$.password") String password) {
         logger.info("用户登入: username={}, password=******", username);
+
         try {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
             if (userDetails.getPassword().equals(password) && userDetails.isEnabled()) {
@@ -56,9 +57,15 @@ public class AuthController {
         SecurityContext context = SecurityContextHolder.getContext();
         Authentication authentication = context.getAuthentication();
         if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName();
             context.setAuthentication(null);
-            logger.info("用户登出: 用户已成功登出, username={}", authentication.getName());
-            return "{\"success\": true}";
+            if (!"anonymousUser".equals(username)) {
+                logger.info("用户登出: 用户已成功登出, username={}", username);
+                return "{\"success\": true}";
+            } else {
+                logger.info("用户登出: 检测到匿名用户");
+                return "{\"success\": true}";
+            }
         } else {
             logger.info("用户登出: 未检测到已登入状态");
             return "{\"success\": false}";
@@ -73,8 +80,13 @@ public class AuthController {
         Authentication authentication = context.getAuthentication();
         if (authentication != null && authentication.isAuthenticated()) {
             String username = authentication.getName();
-            logger.info("状态获取: 检测到已登入用户, username={}", username);
-            return "{\"success\": true, \"username\": \"" + username + "\"}";
+            if (!"anonymousUser".equals(username)) {
+                logger.info("状态获取: 检测到已登入用户, username={}", username);
+                return "{\"success\": true, \"username\": \"" + username + "\"}";
+            } else {
+                logger.info("状态获取: 检测到匿名用户");
+                return "{\"success\": false}";
+            }
         } else {
             logger.info("状态获取: 未检测到已登入状态");
             return "{\"success\": false}";
