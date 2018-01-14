a3d33d1cfa8596a45fb3440367d6e70888393217
fuhaiwei <fuhaiwei@126.com>
2018-01-14 13:15:34 +0800

    feat: add sakura controller

--- src/main/java/mingzuozhibi/persist/model/discList/DiscListRepository.java
+++ src/main/java/mingzuozhibi/persist/model/discList/DiscListRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DiscListRepository extends PagingAndSortingRepository<DiscList, Long> {
 
     DiscList findByName(@Param("name") String name);
+
+    List<DiscList> findBySakura(@Param("sakura") boolean sakura);
 
 }
