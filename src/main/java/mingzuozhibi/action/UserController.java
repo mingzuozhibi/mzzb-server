ca54c9d1ed71e361407d15478bd701add6a32efa
fuhaiwei <fuhaiwei@126.com>
2018-02-12 01:01:42 +0800

    feat: add post /api/users api

--- src/main/java/mingzuozhibi/action/UserController.java
+++ src/main/java/mingzuozhibi/action/UserController.java
@@ -2,9 +2,11 @@ package mingzuozhibi.action;
 
 import mingzuozhibi.persist.User;
 import mingzuozhibi.support.Dao;
+import mingzuozhibi.support.JsonArg;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +22,18 @@ public class UserController extends BaseController {
             array.put(user.toJSON());
         });
         return objectResult(array);
+    }
+
+    @PostMapping(value = "/api/users", produces = CONTENT_TYPE)
+    public String save(
+            @JsonArg("$.username") String username,
+            @JsonArg("$.password") String password) {
+        if (dao.lookup(User.class, "username", username) != null) {
+            return errorMessage("用户名已被注册");
+        }
+        User user = new User(username, password);
+        dao.save(user);
+        return objectResult(user.toJSON());
     }
 
 }
