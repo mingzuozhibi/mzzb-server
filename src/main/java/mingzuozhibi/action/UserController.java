fd9e55088d3fcea31e18272bde10b7b11d8fbb0e
fuhaiwei <fuhaiwei@126.com>
2018-02-12 13:18:44 +0800

    feat: add post /api/users/{id} api

--- src/main/java/mingzuozhibi/action/UserController.java
+++ src/main/java/mingzuozhibi/action/UserController.java
@@ -6,6 +6,7 @@ import mingzuozhibi.support.JsonArg;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,21 @@ public class UserController extends BaseController {
         }
         User user = new User(username, password);
         dao.save(user);
+        return objectResult(user.toJSON());
+    }
+
+    @PostMapping(value = "/api/users/{id}", produces = CONTENT_TYPE)
+    public String editUser(
+            @PathVariable("id") Long id,
+            @JsonArg("$.username") String username,
+            @JsonArg("$.password") String password,
+            @JsonArg("$.enabled") boolean enabled) {
+        User user = dao.get(User.class, id);
+        user.setUsername(username);
+        user.setEnabled(enabled);
+        if (password != null && !password.isEmpty()) {
+            user.setPassword(password);
+        }
         return objectResult(user.toJSON());
     }
 
