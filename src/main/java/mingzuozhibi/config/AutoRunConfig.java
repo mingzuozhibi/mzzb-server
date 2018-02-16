f6804b1986a045dfff8c75707d19db2386657e3b
fuhaiwei <fuhaiwei@126.com>
2018-02-16 23:34:00 +0800

    chore: Update AutoRunConfig Logger

--- src/main/java/mingzuozhibi/config/AutoRunConfig.java
+++ src/main/java/mingzuozhibi/config/AutoRunConfig.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 
 @Service
 public class AutoRunConfig {
@@ -46,13 +47,15 @@ public class AutoRunConfig {
         try {
             sakuraSpeedSpider.fetch();
             LOGGER.debug("成功更新sakura数据");
-        } catch (IOException e) {
+        } catch (SocketTimeoutException e) {
             if (retry > 0) {
-                LOGGER.debug("更新sakura数据时抛出一个错误", e);
+                LOGGER.debug("抓取Sakura网站超时，正在进行重试");
                 fetchSakuraSpeedData(retry - 1);
             } else {
-                LOGGER.warn("更新sakura数据失败", e);
+                LOGGER.warn("抓取Sakura网站超时, 更新sakura数据失败");
             }
+        } catch (IOException e) {
+            LOGGER.warn("抓取Sakura网站遇到意外错误", e);
         }
     }
 
