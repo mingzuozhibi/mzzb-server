e5456dfccafa027ea2e601642140cdc4fd86f7a6
fuhaiwei <fuhaiwei@126.com>
2018-02-17 11:06:00 +0800

    refactor: Change inner class to top class

---
+++ src/main/java/mingzuozhibi/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package mingzuozhibi.security;
+
+import mingzuozhibi.action.BaseController;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint extends BaseController implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        if (LOGGER.isWarnEnabled()) {
+            LOGGER.warn("[{}]越权访问:{}:{}", request.getRemoteAddr(), request.getMethod(), request.getRequestURI());
+        }
+        response.getWriter().write(errorMessage(authException.getMessage()));
+        response.flushBuffer();
+    }
+}
---
+++ src/main/java/mingzuozhibi/security/CustomLogoutHandler.java
@@ -0,0 +1,29 @@
+package mingzuozhibi.security;
+
+import mingzuozhibi.action.BaseController;
+import mingzuozhibi.action.SessionController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomLogoutHandler extends BaseController implements LogoutHandler, LogoutSuccessHandler {
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("[{}][{}]登出成功", getWebDetails().getRemoteAddress(), getCurrentName());
+        }
+    }
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.getWriter().write(objectResult(SessionController.getJSON(null)));
+        response.flushBuffer();
+    }
+}
