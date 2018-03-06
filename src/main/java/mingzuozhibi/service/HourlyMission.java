afbc8979e63e9852293ff829371e7d527c29c800
fuhaiwei <fuhaiwei@126.com>
2018-03-06 22:31:50 +0800

    feat(auto-run): Add computeTotalPt

--- src/main/java/mingzuozhibi/service/HourlyMission.java
+++ src/main/java/mingzuozhibi/service/HourlyMission.java
@@ -6,6 +6,7 @@ import mingzuozhibi.persist.disc.Disc.UpdateType;
 import mingzuozhibi.persist.disc.Record;
 import mingzuozhibi.persist.disc.Sakura;
 import mingzuozhibi.support.Dao;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import static mingzuozhibi.persist.disc.Sakura.ViewType.SakuraList;
@@ -85,6 +87,7 @@ public class HourlyMission {
             @SuppressWarnings("unchecked")
             List<Disc> discs = session.createCriteria(Disc.class)
                     .add(Restrictions.ne("updateType", UpdateType.Sakura))
+                    .add(Restrictions.ne("updateType", UpdateType.None))
                     .add(Restrictions.gt("releaseDate", date.minusDays(7)))
                     .list();
 
@@ -101,7 +104,73 @@ public class HourlyMission {
                 }
                 record.setRank(hour, disc.getThisRank());
             });
+
+            LOGGER.info("[定时任务][计算非Sakura碟片PT][碟片数量为:{}]", discs.size());
+
+            discs.forEach(disc -> {
+                @SuppressWarnings("unchecked")
+                List<Record> records = session.createCriteria(Record.class)
+                        .add(Restrictions.eq("disc", disc))
+                        .add(Restrictions.lt("date", disc.getReleaseDate()))
+                        .addOrder(Order.asc("date"))
+                        .list();
+                disc.setTotalPt((int) computeTotalPt(disc, records));
+            });
         });
+    }
+
+    private double computeTotalPt(Disc disc, List<Record> records) {
+        AtomicReference<Integer> lastRank = new AtomicReference<>();
+        return records.stream()
+                .mapToDouble(record -> computeRecordPt(disc, record, lastRank))
+                .sum();
+    }
+
+    private double computeRecordPt(Disc disc, Record record, AtomicReference<Integer> lastRank) {
+        double recordPt = 0d;
+        for (int i = 0; i < 24; i++) {
+            Integer rank = record.getRank(i + 1);
+            if (rank == null) {
+                rank = lastRank.get();
+            } else {
+                lastRank.set(rank);
+            }
+            if (rank != null) {
+                recordPt += computeHourPt(disc, rank);
+            }
+        }
+        return recordPt;
+    }
+
+    private double computeHourPt(Disc disc, Integer rank) {
+        switch (disc.getDiscType()) {
+            case Cd:
+                return computePt(150, 5.25, rank);
+            case Other:
+                return 0d;
+            default:
+                if (disc.getTitle().contains("Blu-ray")) {
+                    if (rank <= 10) {
+                        return computePt(100, 3.2, rank);
+                    } else if (rank <= 20) {
+                        return computePt(100, 3.3, rank);
+                    } else if (rank <= 50) {
+                        return computePt(100, 3.4, rank);
+                    } else if (rank <= 100) {
+                        return computePt(100, 3.6, rank);
+                    } else if (rank <= 300) {
+                        return computePt(100, 3.8, rank);
+                    } else {
+                        return computePt(100, 3.9, rank);
+                    }
+                } else {
+                    return computePt(100, 4.2, rank);
+                }
+        }
+    }
+
+    private static double computePt(int div, double base, int rank) {
+        return div / Math.exp(Math.log(rank) / Math.log(base));
     }
 
     private boolean isReleasedSevenDays(Disc disc) {
