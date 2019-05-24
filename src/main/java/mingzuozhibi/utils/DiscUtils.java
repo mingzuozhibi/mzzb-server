48fd3605a0bc5c3bab293317eb4caf068d63f037
fuhaiwei <fuhaiwei@126.com>
2019-05-24 19:11:23 +0800

    feat(disc-spider): add first update asins

--- src/main/java/mingzuozhibi/utils/DiscUtils.java
+++ src/main/java/mingzuozhibi/utils/DiscUtils.java
@@ -4,6 +4,7 @@ import mingzuozhibi.persist.disc.Disc;
 import org.hibernate.Session;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -22,14 +23,24 @@ public abstract class DiscUtils {
 
     public static Set<String> needUpdateAsins(Session session) {
         Set<String> asins = new LinkedHashSet<>();
+        Set<String> first = new LinkedHashSet<>();
+        LocalDateTime tenHours = LocalDateTime.now().minusHours(10);
 
         SakuraUtils.sakurasOrderByDescKey(session).forEach(sakura -> {
             sakura.getDiscs().stream()
                     .filter(needUpdateDisc())
-                    .map(Disc::getAsin)
-                    .forEach(asins::add);
+                    .forEach(disc -> {
+                        LocalDateTime lastUpdate = disc.getUpdateTime();
+                        if (lastUpdate == null || lastUpdate.isBefore(tenHours)) {
+                            first.add(disc.getAsin());
+                        } else {
+                            asins.add(disc.getAsin());
+                        }
+                    });
         });
-        return asins;
+
+        first.addAll(asins);
+        return first;
     }
 
     private static Predicate<Disc> needRecordDisc() {
