247d9223c52deb71d8c1253e4a0d240b864faf59
fuhaiwei <fuhaiwei@126.com>
2019-08-25 21:50:28 +0800

    feat(admin): Update fetchCount

--- src/main/java/mingzuozhibi/action/AdminController.java
+++ src/main/java/mingzuozhibi/action/AdminController.java
@@ -1,5 +1,6 @@
 package mingzuozhibi.action;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +10,21 @@ import static mingzuozhibi.utils.DiscUtils.needUpdateAsins;
 @RestController
 public class AdminController extends BaseController {
 
-    @Transactional
-    @GetMapping(value = "/api/admin/fetchCount")
-    public String fetchCount() {
-        return objectResult(needUpdateAsins(dao.session()).size());
-    }
-
     @Deprecated
     @Transactional
+    @PreAuthorize("hasRole('BASIC')")
     @GetMapping(value = "/api/discs/activeCount")
     public String deprecatedFetchCount() {
         return fetchCount();
+    }
+
+    @Transactional
+    @PreAuthorize("hasRole('BASIC')")
+    @GetMapping(value = "/api/admin/fetchCount")
+    public String fetchCount() {
+        int fetchCount = needUpdateAsins(dao.session()).size();
+        debugRequest("[fetchCount:{}]", fetchCount);
+        return objectResult(fetchCount);
     }
 
 }
