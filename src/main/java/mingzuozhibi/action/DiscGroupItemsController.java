b45caa1c9d739cdef63ebcaa9cd83c3f22f38cb4
fuhaiwei <fuhaiwei@126.com>
2019-08-27 10:35:04 +0800

    feat(sakura): Move findDiscs()

--- src/main/java/mingzuozhibi/action/DiscGroupItemsController.java
+++ src/main/java/mingzuozhibi/action/DiscGroupItemsController.java
@@ -2,15 +2,66 @@ package mingzuozhibi.action;
 
 import mingzuozhibi.persist.disc.Disc;
 import mingzuozhibi.persist.disc.Sakura;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class DiscGroupItemsController extends BaseController {
+
+    @Deprecated
+    @Transactional
+    @GetMapping(value = "/api/sakuras/key/{key}/discs", produces = MEDIA_TYPE)
+    public String findDiscsDeprecated(
+            @PathVariable String key,
+            @RequestParam(required = false) String discColumns) {
+        return findDiscs(key, discColumns);
+    }
+
+    @Transactional
+    @GetMapping(value = "/api/discGroups/key/{key}/discs", produces = MEDIA_TYPE)
+    public String findDiscs(
+            @PathVariable String key,
+            @RequestParam(required = false) String discColumns) {
+        Sakura sakura = dao.lookup(Sakura.class, "key", key);
+        if (sakura == null) {
+            if (LOGGER.isWarnEnabled()) {
+                warnRequest("[获取列表碟片失败][指定的列表索引不存在][Key={}]", key);
+            }
+            return errorMessage("指定的列表索引不存在");
+        }
+
+        JSONObject result = sakura.toJSON();
+        result.put("discs", buildDiscs(sakura, discColumns));
+
+        if (LOGGER.isDebugEnabled()) {
+            debugRequest("[获取列表碟片成功][列表标题={}][碟片数量={}]",
+                    sakura.getTitle(), sakura.getDiscs().size());
+        }
+        return objectResult(result);
+    }
+
+    private JSONArray buildDiscs(Sakura sakura, String columns) {
+        JSONArray discs = new JSONArray();
+        if (columns == null) {
+            sakura.getDiscs().forEach(disc -> {
+                discs.put(disc.toJSON());
+            });
+        } else {
+            Set<String> columnSet = Stream.of(columns.split(","))
+                    .collect(Collectors.toSet());
+            sakura.getDiscs().forEach(disc -> {
+                discs.put(disc.toJSON(columnSet));
+            });
+        }
+        return discs;
+    }
 
     @Deprecated
     @Transactional
