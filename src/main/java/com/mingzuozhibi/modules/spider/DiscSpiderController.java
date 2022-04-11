aea944c01e525545a38551bc880c527277526dec
fuhaiwei <fuhaiwei@126.com>
2022-04-11 19:04:10 +0800

    fix: no date warning and log name

--- src/main/java/com/mingzuozhibi/modules/spider/DiscSpiderController.java
+++ src/main/java/com/mingzuozhibi/modules/spider/DiscSpiderController.java
@@ -96,7 +96,7 @@ public class DiscSpiderController extends BaseController2 {
         }
         Disc disc = createDisc(asin, discUpdate);
         jmsService.sendDiscTrack(disc.getAsin(), disc.getTitle());
-        jmsMessage.success(logCreate("碟片", disc.getTitle(), asin));
+        jmsMessage.success(logCreate("碟片", disc.getTitle(), disc.getLogName()));
         return dataResult(disc);
     }
 
@@ -109,7 +109,8 @@ public class DiscSpiderController extends BaseController2 {
             .map(date -> LocalDate.parse(date, formatter))
             .orElseGet(LocalDate::now);
         Disc disc = new Disc(asin, title, discType, releaseDate);
-        jmsMessage.warning("创建碟片时未发现发售日期，碟片=" + disc.getLogName());
+        if (releaseDate.equals(LocalDate.now()))
+            jmsMessage.warning("创建碟片时未发现发售日期，碟片=" + disc.getLogName());
         return discRepository.save(disc);
     }
 
