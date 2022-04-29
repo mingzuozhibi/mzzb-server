ef1a5aa07df496525962e65a57e7a5e366dc3643
fuhaiwei <fuhaiwei@126.com>
2022-04-29 10:34:50 +0800

    feat: update jackson config

--- src/main/java/com/mingzuozhibi/commons/gson/JacksonConfig.java
+++ src/main/java/com/mingzuozhibi/commons/gson/JacksonConfig.java
@@ -1,7 +1,8 @@
 package com.mingzuozhibi.commons.gson;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -10,7 +11,9 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 import java.time.*;
-import java.time.format.DateTimeFormatter;
+
+import static com.mingzuozhibi.commons.utils.FormatUtils.fmtDate;
+import static com.mingzuozhibi.commons.utils.MyTimeUtils.*;
 
 @Configuration
 public class JacksonConfig {
@@ -19,44 +22,53 @@ public class JacksonConfig {
     public Jackson2ObjectMapperBuilderCustomizer customJackson() {
         return builder -> {
             builder.serializationInclusion(Include.NON_NULL);
-            builder.modulesToInstall(getSimpleModule());
+            SimpleModule module = new SimpleModule();
+
+            withLocalDateTime(module);
+            withLocalDate(module);
+            withInsatnt(module);
+
+            builder.modulesToInstall(module);
         };
     }
 
-    private SimpleModule getSimpleModule() {
-        SimpleModule module = new SimpleModule();
+    private void withLocalDateTime(SimpleModule module) {
+        module.addSerializer(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
+            public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                gen.writeNumber(toEpochMilli(value));
+            }
+        });
+        module.addDeserializer(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+            public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+                return ofEpochMilli(p.getLongValue());
+            }
+        });
+    }
+
+    private void withLocalDate(SimpleModule module) {
+        module.addSerializer(LocalDate.class, new JsonSerializer<LocalDate>() {
+            public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                gen.writeString(value.format(fmtDate));
+            }
+        });
+        module.addDeserializer(LocalDate.class, new JsonDeserializer<LocalDate>() {
+            public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+                return LocalDate.parse(p.getText(), fmtDate);
+            }
+        });
+    }
+
+    private void withInsatnt(SimpleModule module) {
         module.addSerializer(Instant.class, new JsonSerializer<Instant>() {
             public void serialize(Instant value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
                 gen.writeNumber(value.toEpochMilli());
             }
         });
         module.addDeserializer(Instant.class, new JsonDeserializer<Instant>() {
-            public Instant deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+            public Instant deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
                 return Instant.ofEpochMilli(p.getLongValue());
             }
         });
-        module.addSerializer(LocalDateTime.class, new JsonSerializer<LocalDateTime>() {
-            public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-                gen.writeNumber(value.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
-            }
-        });
-        module.addDeserializer(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
-            public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-                return Instant.ofEpochMilli(p.getLongValue()).atZone(ZoneId.systemDefault()).toLocalDateTime();
-            }
-        });
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/d");
-        module.addSerializer(LocalDate.class, new JsonSerializer<LocalDate>() {
-            public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-                gen.writeString(value.format(formatter));
-            }
-        });
-        module.addDeserializer(LocalDate.class, new JsonDeserializer<LocalDate>() {
-            public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-                return LocalDate.parse(p.getText(), formatter);
-            }
-        });
-        return module;
     }
 
 }
