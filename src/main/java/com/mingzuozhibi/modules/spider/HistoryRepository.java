fc66ef932f7afb200738779eb16941ea6fb71df1
fuhaiwei <fuhaiwei@126.com>
2022-05-19 17:16:31 +0800

    feat: Update some repository

--- src/main/java/com/mingzuozhibi/modules/spider/HistoryRepository.java
+++ src/main/java/com/mingzuozhibi/modules/spider/HistoryRepository.java
@@ -1,17 +1,15 @@
 package com.mingzuozhibi.modules.spider;
 
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
-    @Modifying
-    @Query(value = "update history " +
-        "set tracked = ?2 " +
-        "where asin = ?1", nativeQuery = true)
-    void setTracked(String asin, boolean tracked);
-
     Optional<History> findByAsin(String asin);
+
+    default void setTracked(String asin, boolean tracked) {
+        findByAsin(asin).ifPresent(history -> history.setTracked(tracked));
+    }
 
 }
