78eadf1a01459099e03c19dd75affc4e2d82798b
fuhaiwei <fuhaiwei@126.com>
2022-07-21 13:38:25 +0800

    feat: Update cleanupModulesMessages()

--- src/main/java/com/mingzuozhibi/modules/admin/AdminService.java
+++ src/main/java/com/mingzuozhibi/modules/admin/AdminService.java
@@ -9,6 +9,7 @@ import com.mingzuozhibi.modules.disc.Disc;
 import com.mingzuozhibi.modules.disc.GroupService;
 import com.mingzuozhibi.modules.record.*;
 import com.mingzuozhibi.modules.user.RememberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import java.time.*;
 import java.util.List;
 import java.util.Set;
 
+@Slf4j
 @Service
 @LoggerBind(Name.SERVER_CORE)
 public class AdminService extends BaseSupport {
@@ -74,34 +76,29 @@ public class AdminService extends BaseSupport {
             int c1 = messageRepository.cleanup(Name.SPIDER_CONTENT, 150, Type.INFO, Type.WARNING);
             int c2 = messageRepository.cleanup(Name.SPIDER_CONTENT, 200);
             count += c1 + c2;
-            if (c1 > 0 || c2 > 0)
-                bind.info("[清理日志][name=%s][size=%d,%d]".formatted(Name.SPIDER_CONTENT, c1, c2));
+            log.info("[清理日志][name=%s][size=%d,%d]".formatted(Name.SPIDER_CONTENT, c1, c2));
         }
         {
             int c1 = messageRepository.cleanup(Name.SPIDER_HISTORY, 150, Type.INFO);
             int c2 = messageRepository.cleanup(Name.SPIDER_HISTORY, 200);
             count += c1 + c2;
-            if (c1 > 0 || c2 > 0)
-                bind.info("[清理日志][name=%s][size=%d,%d]".formatted(Name.SPIDER_HISTORY, c1, c2));
+            log.info("[清理日志][name=%s][size=%d,%d]".formatted(Name.SPIDER_HISTORY, c1, c2));
         }
         {
             int c1 = messageRepository.cleanup(Name.SERVER_DISC, 150, Type.INFO);
             int c2 = messageRepository.cleanup(Name.SERVER_DISC, 200);
             count += c1 + c2;
-            if (c1 > 0 || c2 > 0)
-                bind.info("[清理日志][name=%s][size=%d,%d]".formatted(Name.SERVER_DISC, c1, c2));
+            log.info("[清理日志][name=%s][size=%d,%d]".formatted(Name.SERVER_DISC, c1, c2));
         }
         {
             int c1 = messageRepository.cleanup(Name.SERVER_CORE, 200);
             count += c1;
-            if (c1 > 0)
-                bind.info("[清理日志][name=%s][size=%d]".formatted(Name.SERVER_CORE, c1));
+            log.info("[清理日志][name=%s][size=%d]".formatted(Name.SERVER_CORE, c1));
         }
         {
             int c1 = messageRepository.cleanup(Name.DEFAULT, 200);
             count += c1;
-            if (c1 > 0)
-                bind.info("[清理日志][name=%s][size=%d]".formatted(Name.DEFAULT, c1));
+            log.info("[清理日志][name=%s][size=%d]".formatted(Name.DEFAULT, c1));
         }
         bind.info("[自动任务][清理日志][共%d条]".formatted(count));
     }
