b0fcdfc0827bf1d1bd48dfbfc20bb15fac3c4481
fuhaiwei <fuhaiwei@126.com>
2022-09-19 21:07:42 +0800

    chore: Move etc some file to dat

--- etc/upgrade_2022-05-14.sql
+++
@@ -1,4 +0,0 @@
-rename table auto_login to remember;
-rename table disc_group to `group`;
-rename table disc_group_discs to group_discs;
-alter table group_discs rename column disc_group_id to group_id;
