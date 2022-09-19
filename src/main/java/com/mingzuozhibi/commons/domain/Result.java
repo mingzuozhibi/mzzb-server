f6a71b604ef4129d37041fd55ba800b992587444
fuhaiwei <fuhaiwei@126.com>
2022-09-19 23:03:07 +0800

    feat: Update domain result

--- src/main/java/com/mingzuozhibi/commons/domain/Result.java
+++ src/main/java/com/mingzuozhibi/commons/domain/Result.java
@@ -1,12 +1,15 @@
 package com.mingzuozhibi.commons.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 @Getter
+@NoArgsConstructor
+@SuppressWarnings("unused")
 public class Result<T> {
 
     private boolean success;
@@ -18,23 +21,15 @@ public class Result<T> {
         return !isSuccess();
     }
 
-    public boolean testData(Predicate<T> predicate) {
+    public boolean test(Predicate<T> predicate) {
         return isSuccess() && predicate.test(getData());
     }
 
-    public <R> Result<R> thenData(Function<T, R> function) {
+    public <R> Result<R> then(Function<T, R> function) {
         if (isSuccess()) {
             return ofData(function.apply(getData()));
         } else {
             return ofError(getMessage());
-        }
-    }
-
-    public static <T> Result<T> ofTask(SearchTask<T> task) {
-        if (task.isSuccess()) {
-            return ofData(task.getData());
-        } else {
-            return ofError(task.getMessage());
         }
     }
 
@@ -53,11 +48,17 @@ public class Result<T> {
     }
 
     public static <T> Result<List<T>> ofPage(List<T> data, ResultPage page) {
-        var result = new Result<List<T>>();
-        result.success = true;
-        result.data = data;
+        var result = ofData(data);
         result.page = page;
         return result;
+    }
+
+    public static <T> Result<T> ofTask(SearchTask<T> task) {
+        if (task.isSuccess()) {
+            return ofData(task.getData());
+        } else {
+            return ofError(task.getMessage());
+        }
     }
 
 }
