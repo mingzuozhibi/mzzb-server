8dbcab131317d671429426eeef97e78d13dcbffe
fuhaiwei <fuhaiwei@126.com>
2022-10-02 02:22:33 +0800

    feat: Add filter messages of date

--- src/main/java/com/mingzuozhibi/modules/core/MessageController.java
+++ src/main/java/com/mingzuozhibi/modules/core/MessageController.java
@@ -7,10 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.*;
 import java.util.List;
 
+import static com.mingzuozhibi.commons.utils.MyTimeUtils.toInstant;
+
+@Transactional
 @RestController
 public class MessageController extends PageController {
 
@@ -21,13 +26,21 @@ public class MessageController extends PageController {
     public String findAll(@PathVariable Name name,
                           @RequestParam(required = false) String search,
                           @RequestParam(required = false) List<Type> types,
+                          @RequestParam(required = false) LocalDate start,
+                          @RequestParam(required = false) LocalDate end,
                           @RequestParam(defaultValue = "1") int page,
                           @RequestParam(defaultValue = "20") int size) {
 
-        var pageRequest = PageRequest.of(page - 1, size, Sort.by(Order.desc("id")));
-        var messages = messageRepository.findBy(name, types, search, pageRequest);
+        var messages = messageRepository.findBy(name, types, search,
+            safeInstance(start, LocalTime.of(0, 0, 0)),
+            safeInstance(end, LocalTime.of(23, 59, 59)),
+            PageRequest.of(page - 1, size, Sort.by(Order.desc("id"))));
         return pageResult(messages);
+    }
 
+    private static Instant safeInstance(LocalDate date, LocalTime time) {
+        if (date == null) return null;
+        return toInstant(LocalDateTime.of(date, time));
     }
 
 }
--- src/main/java/com/mingzuozhibi/modules/core/MessageRepository.java
+++ src/main/java/com/mingzuozhibi/modules/core/MessageRepository.java
@@ -8,13 +8,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.criteria.Predicate;
+import java.time.Instant;
 import java.util.*;
 
+@Transactional
 public interface MessageRepository extends JpaRepository<Message, Long>, JpaSpecificationExecutor<Message> {
 
-    default Page<Message> findBy(Name name, List<Type> types, String search, Pageable pageable) {
+    default Page<Message> findBy(Name name, List<Type> types, String search,
+                                 Instant start, Instant end, Pageable pageable) {
         return findAll((Specification<Message>) (root, query, cb) -> {
             var array = new ArrayList<Predicate>();
             array.add(cb.equal(root.get("name"), name));
@@ -24,26 +28,35 @@ public interface MessageRepository extends JpaRepository<Message, Long>, JpaSpec
             if (StringUtils.isNotBlank(search)) {
                 array.add(cb.like(root.get("text"), "%" + search + "%"));
             }
+            if (start != null) {
+                array.add(cb.greaterThan(root.get("createOn"), start));
+            }
+            if (end != null) {
+                array.add(cb.lessThan(root.get("createOn"), end));
+            }
             return query.where(array.toArray(new Predicate[0])).getRestriction();
         }, pageable);
     }
 
-    default int cleanup(Name name, int size, Type... typeIn) {
+    default int cleanup(Name name, int page, Type... typeIn) {
         if (typeIn.length == 0) typeIn = Type.values();
         var types = Arrays.stream(typeIn).mapToInt(Enum::ordinal).toArray();
-        var id = findTargetId(name.name(), types, size * 20);
+        var id = findTargetId(name.name(), page * 20);
         if (id == null) return 0;
         return deleteByTargetId(name.name(), types, id);
     }
 
-    @Query(value = "select id from message " +
-        "where name = ?1 and type in ?2 " +
-        "order by id desc limit ?3,1", nativeQuery = true)
-    Long findTargetId(String name, int[] types, int size);
+    @Query(value = "select id from message" +
+        " where name = ?1" +
+        " order by id desc" +
+        " limit ?2,1", nativeQuery = true)
+    Long findTargetId(String name, int size);
 
     @Modifying
-    @Query(value = "delete from message " +
-        "where name = ?1 and type in ?2 and id <= ?3", nativeQuery = true)
+    @Query(value = "delete from message" +
+        " where name = ?1" +
+        " and type in ?2" +
+        " and id <= ?3", nativeQuery = true)
     int deleteByTargetId(String name, int[] types, Long id);
 
 }
