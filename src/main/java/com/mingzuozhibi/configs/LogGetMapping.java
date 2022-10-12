672b11712d25340222eeacad37cdde756212b695
fuhaiwei <fuhaiwei@126.com>
2022-10-12 19:30:20 +0800

    feat: LogGetMapping output argNames

--- src/main/java/com/mingzuozhibi/configs/LogGetMapping.java
+++ src/main/java/com/mingzuozhibi/configs/LogGetMapping.java
@@ -1,15 +1,19 @@
 package com.mingzuozhibi.configs;
 
+import com.mingzuozhibi.commons.base.BaseSupport;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.StopWatch;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.util.StringJoiner;
 
 @Slf4j
 @Aspect
 @Component
-public class LogGetMapping {
+public class LogGetMapping extends BaseSupport {
 
     @Pointcut(value = "@annotation(org.springframework.web.bind.annotation.GetMapping)")
     public void pointCut() {
@@ -17,12 +21,31 @@ public class LogGetMapping {
 
     @Around(value = "pointCut()")
     public Object aroundPringLog(ProceedingJoinPoint joinPoint) throws Throwable {
-        var sw = new StopWatch();
-        sw.start();
+        var stopWatch = new StopWatch();
+        stopWatch.start();
         var proceed = joinPoint.proceed(joinPoint.getArgs());
-        sw.stop();
-        log.debug("Log @GetMapping: %s %dms".formatted(joinPoint.getSignature(), sw.getTime()));
+        stopWatch.stop();
+        log.debug("Log @GetMapping: %s %s %dms".formatted(
+            joinPoint.getSignature(),
+            formatArgs(joinPoint),
+            stopWatch.getTime()
+        ));
         return proceed;
+    }
+
+    private String formatArgs(ProceedingJoinPoint joinPoint) {
+        var args = joinPoint.getArgs();
+        if (args.length == 0) return "[<noargs>]";
+        try {
+            var names = ((MethodSignature) joinPoint.getSignature()).getParameterNames();
+            var joiner = new StringJoiner(",", "[", "]");
+            for (var i = 0; i < args.length; i++) {
+                joiner.add("%s=%s".formatted(names[i], gson.toJson(args[i])));
+            }
+            return joiner.toString();
+        } catch (Exception e) {
+            return "[<error>]";
+        }
     }
 
 }
